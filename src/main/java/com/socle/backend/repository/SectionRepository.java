1d889b761d1d74ada465b1e4c45f1d3d9dbb4c5e
manitou4545 <139740796+manitou4545@users.noreply.github.com>
2026-08-06 02:24:49 +0100

    Create SectionRepository interface for SectionEntity

---
+++ src/main/java/com/socle/backend/repository/SectionRepository.java
@@ -0,0 +1,7 @@
+package com.socle.backend.repository;
+
+import com.socle.backend.model.SectionEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SectionRepository extends JpaRepository<SectionEntity, Long> {
+}
